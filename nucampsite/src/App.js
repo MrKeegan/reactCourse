ef369787345c61ac09cbf52312e733c2b5855fb9
Tony Keegan <tlkeegan@outlook.com>
2020-12-22 10:18:00 -0800

    React States

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,23 +1,25 @@
 import React, {Component} from 'react';
-import {Navbar,VanvarBrand} from 'reactstrap';
+import {Navbar,NavbarBrand} from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
       return (
-          <div className="App">
+          <div key ={campsite.id} className="App">
               <Navbar dark color="primary">
               <div className="container">
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
               </Navbar>
+              <Directory />
           </div>
       );
   }
 }
 
-export default App;
+
 
 function App() {
   return (
