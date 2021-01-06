718d6a5ad873d377634373347d376eb834b0273e
Tony Keegan <tlkeegan@outlook.com>
2021-01-05 17:40:55 -0800

    Intro to Redux

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -4,25 +4,22 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from "./HeaderComponent";
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import {Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
 import Footer from './FooterComponent';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      comments: COMMENTS,
-      partners: PARTNERS,
-      promotions: PROMOTIONS
 
 
-      };
-
+const mapStateToProps = state => {
+  return{
+    campsites: state.campsites,
+    comments: state.comments,
+    partners: state.partners,
+    promotions: state.promotions
   }
+}
+class Main extends Component {
+  
+
 
  
 
@@ -32,9 +29,9 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home
-          campsite ={this.state.campsites.filters(campsite => campsite.featured)[0]}
-          promotion={this.state.promotions.filters(campsite => campsite.promotion)[0]}
-          partner={this.state.promotions.filters(campsite => campsite.partner)[0]}
+          campsite ={this.props.campsites.filters(campsite => campsite.featured)[0]}
+          promotion={this.props.promotions.filters(campsite => campsite.promotion)[0]}
+          partner={this.props.promotions.filters(campsite => campsite.partner)[0]}
         />
       );
     }
@@ -42,8 +39,8 @@ class Main extends Component {
     const CampsiteWithId = ({match}) =>{
       return (
         <CampsiteInfo
-          campsite = {this.state.campsites.filter(campsite => campsite.id === +match.params.CampsiteWithId)[0]}
-          comments = {this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}/>
+          campsite = {this.props.campsites.filter(campsite => campsite.id === +match.params.CampsiteWithId)[0]}
+          comments = {this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}/>
       );
     }
 
@@ -52,7 +49,7 @@ class Main extends Component {
         <Header/>
         <Switch>
           <Route path='/home' component={HomePage}/>
-          <Route exact path='/directory' render ={() => <Directory campsites={this.state.campsites} />}/>
+          <Route exact path='/directory' render ={() => <Directory campsites={this.props.campsites} />}/>
           <Route path ='/directory/:campsiteId' component={CampsiteWithId}/>
           <Route exact path = '/contactus' component = {Contact}/>
           <Redirect to ='/home'/>
@@ -63,4 +60,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStatetoProps)(Main));
